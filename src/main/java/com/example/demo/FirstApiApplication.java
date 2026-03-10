d0ba5fc44fb868ce6212967070e609fcbad856f3
deveploper99 <rtxdeveploper99@gmail.com>
2026-03-10 15:00:02 +0600

    coonect and test jpa

--- src/main/java/com/example/demo/FirstApiApplication.java
+++ src/main/java/com/example/demo/FirstApiApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class FirstApiApplication {
@@ -9,5 +13,15 @@ public class FirstApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstApiApplication.class, args);
 	}
+
+    @Bean
+    CommandLineRunner testDatabsesConnection(DataSource dataSource){
+        return args -> {
+            System.out.println("Databse connection Successfully");
+            System.out.println("Databse url "+dataSource.getConnection().getMetaData().getURL());
+
+        };
+
+    }
 
 }
